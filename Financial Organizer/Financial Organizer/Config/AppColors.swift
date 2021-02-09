44f67daddb9b961a4adfd40c8b012a8602916cf8
Felipe Santana <felipepipa16@hotmail.com>
2021-02-09 09:18:20 -0300

    Project created and configured

---
+++ Financial Organizer/Financial Organizer/Config/AppColors.swift
@@ -0,0 +1,27 @@
+//
+//  AppColors.swift
+//  Financial Organizer
+//
+//  Created by Felipe Santana on 09/02/21.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    static let textDarkGrey = UIColor.init(displayP3Red: 61/255, green: 61/255, blue: 61/255, alpha: 1)
+    
+    static let backgroundGrey = UIColor.init(displayP3Red: 64/255, green: 64/255, blue: 64/255, alpha: 1)
+    
+    static let incomeGreen = UIColor.init(displayP3Red: 98/255, green: 168/255, blue: 49/255, alpha: 1)
+    
+    static let expenseRed = UIColor.init(displayP3Red: 215/255, green: 48/255, blue: 60/255, alpha: 1)
+    
+    static let tableTextWhite = UIColor.init(displayP3Red: 237/255, green: 237/255, blue: 237/255, alpha: 1)
+    
+    static let paidGreen = UIColor.init(displayP3Red: 24/255, green: 169/255, blue: 3/255, alpha: 1)
+    
+    static let pendingYellow = UIColor.init(displayP3Red: 250/255, green: 196/255, blue: 0/255, alpha: 1)
+    
+}
---
+++ Financial Organizer/Financial Organizer/Config/Enums.swift
@@ -0,0 +1,8 @@
+//
+//  Enums.swift
+//  Financial Organizer
+//
+//  Created by Felipe Santana on 09/02/21.
+//
+
+import Foundation
